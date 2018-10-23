187df57fa2ca3e256d685b0577a7e7aac4828f35
s4hil-k <44373600+s4hil-k@users.noreply.github.com>
2018-10-23 02:23:29 -0600

    Update gpi-db-connect.php

--- gpi-db-connect.php
+++ gpi-db-connect.php
@@ -1,6 +1,6 @@
 <?
 try {
-	$gpi_database_connection = new PDO("mysql:host=mysql.nrdevsites.com;dbname=ipullupull_inventory_mgt", "ipullupull_dev", 'k8$o0Upe');
+	$gpi_database_connection = new PDO("mysql:host=localhost;dbname=ipullupull_inventory_mgt", "ipullupull_dev", 'k8$o0Upe');
 	$gpi_database_connection->SetAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 	$gpi_get_all_records = 'SELECT * FROM `crush_inventory_photos`';
